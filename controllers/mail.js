515c55d046fff4a0e6c95273d88ac860e4afd4e5
Suvarna <suvarna.pawar812@gmail.com>
2022-07-10 11:48:19 +0530

    mail

---
+++ controllers/mail.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+
+const sendEmail=(email,message)=>{
+
+    var message = {
+        from: "suvarna.pawar812@gmail.com",
+        to: email,
+        subject: "SUCCESS REGISTRATION",
+        text: message,
+        html: `<p>${message}</p>`
+      };
+   const transporter= nodemailer.createTransport({
+        host: "smtp-relay.sendinblue.com",  // host of your smtp provider
+        port: 587,
+        secure: false, // upgrade later with STARTTLS
+      
+        auth: {
+          user: "suvarna.pawar812@gmail.com",
+          pass: "qsSvKnOR1260BZJ9",
+        },
+      });
+
+      transporter.sendMail(message,function(err,data){
+        console.log(err);
+
+        console.log(data);
+
+      })
+
+}
+
+module.exports={sendEmail};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,13 @@
 const express=require("express");
 const authrouter=express.Router();
-
+const{ sendEmail }= require("../controllers/mail")
 const {signUp, login}=require("../controllers/auth.controller");
 const {encryptPassword} =require("../middlewares/auth.middleware");
 
 authrouter.post("/signup",encryptPassword,signUp);
 authrouter.post("/login",login);
-
+authrouter.get("/sendEmail",(req,res)=>{
+    sendEmail("suvarna.pawar812@gmail.com","this is a test email to test something")
+    res.send("worked");
+});
 module.exports=authrouter;
